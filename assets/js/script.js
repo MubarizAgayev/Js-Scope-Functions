68594e90360149bb078b722c37601251211ea223
mubarizra@code.edu.az <mubarizra@code.edu.az>
2023-02-07 20:53:24 +0400

    ScopeFunctions

---
+++ assets/js/script.js
@@ -0,0 +1,137 @@
+"use disrict"
+
+// Task1
+// let n = 21
+// isDivided(n);
+// function isDivided(n){
+//     if(n%3==0 && n%7==0){
+//         console.log("bolunur");
+//     }else{
+//         console.log("bolunmur");
+//     }
+// }
+// let Divide = (n) =>{
+//     if(n%3==0 && n%7==0){
+//         console.log("bolunur");
+//     }else{
+//         console.log("bolunmur");
+//     }
+// }
+// Divide(n)
+
+//Task2
+// let n =5;
+// Factorial(n);
+// function Factorial(n){
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result*=i;
+//     }
+//     console.log(result);
+// }
+
+// let Fact = (n) =>{
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result*=i;
+//     }
+//     console.log(result);
+// }
+// Fact(n);
+
+
+//Task3
+// let nums=[1,2,3,4,5,6]
+// getSum(nums);
+// function getSum(nums){
+//     let sum =0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if(nums[i]%2==0){
+//             sum+=nums[i]**2
+//         }
+//     }
+//     console.log(sum);
+// }
+
+// let getFilteredNumsSum = (nums) =>{
+//     let sum =0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if(nums[i]%2==0){
+//             sum+=nums[i]**2
+//         }
+//     }
+//     console.log(sum);
+// }
+// getFilteredNumsSum(nums);
+
+
+//Task4
+// let email="cavid@code.edu.az";
+// let password="12345"
+// Login(email,password)
+// function Login(email,password){
+//     if(email=="cavid@code.edu.az" && password=="12345"){
+//         console.log("Girish olundu");
+//     }else{
+//         console.log("Mail or password is wrong");
+//     }
+// }
+
+// let getLogin = (email,password) =>{
+//     if(email=="cavid@code.edu.az" && password=="12345"){
+//         console.log("Girish olundu");
+//     }else{
+//         console.log("Mail or password is wrong");
+//     }
+// }
+// getLogin(email,password)
+
+
+//Task5
+// let numbers=[3,4,5,6,7,8,9]
+// getOddNums(numbers)
+// function getOddNums(numbers){
+//     let result = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         if(numbers[i]%2==1){
+//             result+=numbers[i]
+//         }
+//     }
+//     console.log(result);
+// }
+
+// let getOddNumbers = (numbers) =>{
+//     let result = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         if(numbers[i]%2==1){
+//             result+=numbers[i]
+//         }
+//     }
+//     console.log(result);
+// }
+// getOddNumbers(numbers)
+
+
+//Task6
+// let nums = [5,6,7,8,9,10,11,12]
+// getCountEvenNums(nums)
+// function getCountEvenNums(nums){
+//     let count=0
+//     for (let i = 0; i < nums.length; i++) {
+//         if(nums[i]%2==0){
+//             count++
+//         }
+//     }
+//     console.log(count);
+// }
+
+// let getCountEvenNumbers = () =>{
+//     let count=0
+//     for (let i = 0; i < nums.length; i++) {
+//         if(nums[i]%2==0){
+//             count++
+//         }
+//     }
+//     console.log(count);
+// }
+// getCountEvenNumbers(nums)
